4f0a882b67cd392ebde321b6a3f25107dfeec9e9
samuelc024 <142014521+samuelc024@users.noreply.github.com>
2023-08-23 05:41:23 -0500

    Create Ejercicio1.py

---
+++ CONDICIONALES/Ejercicio1.py
@@ -0,0 +1,8 @@
+num = float(input("Ingrese un número: "))
+
+if num > 0:
+    print("El número", num, "es positivo.")
+elif num < 0:
+    print("El número", num, "es negativo.")
+else:
+    print("El número es cero.")
